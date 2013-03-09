9daee5cabed20d4f7f1121bf8f01964d594cb5df
aldenhart <alden08@harts.org>
2013-03-09 17:08:25 -0500

    initial commit

---
+++ TinyG3/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+cmsis\src\startup_sam3xa.c
+
+cmsis\src\system_sam3xa.c
+
+TinyG3.cpp
+
---
+++ TinyG3/TinyG3.cpp
@@ -0,0 +1,24 @@
+/*
+ * TinyG3.cpp
+ *
+ * Created: 3/9/2013 3:15:40 PM
+ *  Author: Administrator
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
