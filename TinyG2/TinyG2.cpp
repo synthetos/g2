dd9af7f1feb15c27267cbe8a1e660c46f8266316
alden.hart <alden08@harts.org>
2013-03-29 18:06:33 -0400

    Added Rob's new project structure as TinyG2 - currently not complete

---
+++ TinyG2/TinyG2.cpp
@@ -0,0 +1,24 @@
+/*
+ * TinyG2.cpp
+ *
+ * Created: 3/29/2013 3:38:03 PM
+ *  Author: Administrator
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
