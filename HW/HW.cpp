df363705239cafb7c06015f5774caa0bfefb0496
alden.hart <alden08@harts.org>
2013-03-31 12:03:39 -0400

    Added HW as a hello world test for ICE debugging

---
+++ HW/HW.cpp
@@ -0,0 +1,26 @@
+/*
+ * HW.cpp
+ *
+ * Created: 3/31/2013 11:19:33 AM
+ *  Author: Administrator
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+/*
+int main(void)
+{
+    // Initialize the SAM system
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
+*/
