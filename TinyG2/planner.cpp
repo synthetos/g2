7d0f6d50266d4a146b3ae210c631700559961875
aldenhart <alden08@harts.org>
2013-10-10 14:22:54 -0400

    020.13 Alignment changes to planner

--- TinyG2/planner.cpp
+++ TinyG2/planner.cpp
@@ -2,8 +2,8 @@
  * planner.cpp - cartesian trajectory planning and motion execution
  * This file is part of the TinyG project
  *
- * Copyright (c) 2013 Alden S. Hart Jr.
- * Copyright (c) 2013 Robert Giseburt
+ * Copyright (c) 2010 - 2013 Alden S. Hart, Jr.
+ * Copyright (c) 2012 - 2013 Rob Giseburt
  *
  * This file ("the software") is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 as published by the
@@ -57,7 +57,6 @@
 #include "plan_arc.h"
 #include "plan_line.h"
 #include "planner.h"
-//#include "spindle.h"
 #include "stepper.h"
 #include "report.h"
 #include "util.h"
@@ -67,6 +66,7 @@ extern "C"{
 #endif
 
 // Allocate planner structures
+
 mpBufferPool_t mb;				// move buffer queue
 mpMoveMasterSingleton_t mm;		// context for line planning
 mpMoveRuntimeSingleton_t mr;	// context for line runtime
@@ -80,8 +80,8 @@ mpMoveRuntimeSingleton_t mr;	// context for line runtime
 #define flag_vector unit		// alias for vector of flags
 
 // execution routines (NB: These are all called from the LO interrupt)
-static uint8_t _exec_dwell(mpBuf_t *bf);
-static uint8_t _exec_command(mpBuf_t *bf);
+static stat_t _exec_dwell(mpBuf_t *bf);
+static stat_t _exec_command(mpBuf_t *bf);
 
 #ifdef __DEBUG
 static uint8_t _get_buffer_index(mpBuf_t *bf); 
@@ -91,7 +91,6 @@ static void _dump_plan_buffer(mpBuf_t *bf);
 /* 
  * planner_init()
  */
-
 void planner_init()
 {
 // If you can can assume all memory has been zeroed by a hard reset you don;t need these next 2 lines
@@ -105,19 +104,30 @@ void planner_init()
 	mp_init_buffers();
 }
 
+/*
+ * mp_assertions() - test assertions, return error code if violation exists
+ */
+stat_t mp_assertions()
+{
+	if ((mb.magic_start  != MAGICNUM) || (mb.magic_end 	 != MAGICNUM)) return (STAT_MEMORY_FAULT);
+	if ((mr.magic_start  != MAGICNUM) || (mr.magic_end 	 != MAGICNUM)) return (STAT_MEMORY_FAULT);
+	if ((arc.magic_start != MAGICNUM) || (arc.magic_end  != MAGICNUM)) return (STAT_MEMORY_FAULT);
+	return (STAT_OK);
+}
+
 /* 
  * mp_flush_planner() - flush all moves in the planner and all arcs
  *
  *	Does not affect the move currently running in mr.
  *	Does not affect mm or gm model positions
  *	This function is designed to be called during a hold to reset the planner
- *	This function should not generally be called; call cm_flush_planner() instead
+ *	This function should not generally be called; call cm_queue_flush() instead
  */
 void mp_flush_planner()
 {
 	cm_abort_arc();
 	mp_init_buffers();
-	cm.motion_state = MOTION_STOP;
+	cm_set_motion_state(MOTION_STOP);
 }
 
 /*
@@ -154,23 +164,20 @@ void mp_set_runtime_position(uint8_t axis, const float position)
  *	Manages run buffers and other details
  */
 
-stat_t mp_exec_move() 
+stat_t mp_exec_move()
 {
 	mpBuf_t *bf;
 
 	if ((bf = mp_get_run_buffer()) == NULL) return (STAT_NOOP);	// NULL means nothing's running
 
 	// Manage cycle and motion state transitions
-	// cycle auto-start for lines only. Add other move types as appropriate.
+	// Cycle auto-start for lines only
 	if (bf->move_type == MOVE_TYPE_ALINE) {
 		if (cm.cycle_state == CYCLE_OFF) cm_cycle_start();
-		if (cm.motion_state == MOTION_STOP) cm.motion_state = MOTION_RUN;		
+		if (cm.motion_state == MOTION_STOP) cm_set_motion_state(MOTION_RUN);
 	}
-//	if ((cm.motion_state == MOTION_STOP) && (bf->move_type == MOVE_TYPE_ALINE)) {
-//		cm.motion_state = MOTION_RUN;
-//	}
-	if (bf->bf_func != NULL) { return (bf->bf_func(bf));} 	// run the move callback in the buffer
-	return (STAT_INTERNAL_ERROR);		// never supposed to get here
+	if (bf->bf_func != NULL) { return (bf->bf_func(bf));} 	// run the move callback in the planner buffer
+	return(cm_alarm(STAT_INTERNAL_ERROR));	// never supposed to get here
 }
 
 /************************************************************************************
@@ -202,12 +209,11 @@ void mp_queue_command(void(*cm_exec)(float[], float[]), float *value, float *fla
 	bf->bf_func = _exec_command;		// callback to planner queue exec function
 	bf->cm_func = cm_exec;				// callback to canonical machine exec function
 
-	for (uint8_t i=0; i<AXES; i++) {
-		bf->value_vector[i] = value[i];
-		bf->flag_vector[i] = flag[i];
+	for (uint8_t axis = AXIS_X; axis < AXES; axis++) {
+		bf->value_vector[axis] = value[axis];
+		bf->flag_vector[axis] = flag[axis];
 	}
 	mp_queue_write_buffer(MOVE_TYPE_COMMAND);
-//	return;
 }
 
 static stat_t _exec_command(mpBuf_t *bf)
@@ -226,7 +232,6 @@ static stat_t _exec_command(mpBuf_t *bf)
  * When the stepper driver sees a dwell it times the dwell on a separate 
  * timer than the stepper pulse timer.
  */
-
 stat_t mp_dwell(float seconds)
 {
 	mpBuf_t *bf;
@@ -356,25 +361,25 @@ void mp_queue_write_buffer(const uint8_t move_type)
 
 mpBuf_t * mp_get_run_buffer() 
 {
-	// condition: fresh buffer; becomes running if queued or pending
+	// CASE: fresh buffer; becomes running if queued or pending
 	if ((mb.r->buffer_state == MP_BUFFER_QUEUED) || 
 		(mb.r->buffer_state == MP_BUFFER_PENDING)) {
 		 mb.r->buffer_state = MP_BUFFER_RUNNING;
 	}
-	// condition: asking for the same run buffer for the Nth time
+	// CASE: asking for the same run buffer for the Nth time
 	if (mb.r->buffer_state == MP_BUFFER_RUNNING) {	// return same buffer
 		return (mb.r);
 	}
-	return (NULL);								// condition: no queued buffers. fail it.
+	return (NULL);								// CASE: no queued buffers. fail it.
 }
 
 void mp_free_run_buffer()						// EMPTY current run buf & adv to next
 {
 	mp_clear_buffer(mb.r);						// clear it out (& reset replannable)
-	mb.r->buffer_state = MP_BUFFER_EMPTY;
-	mb.r = mb.r->nx;							 // advance to next run buffer
+//	mb.r->buffer_state = MP_BUFFER_EMPTY;		// redundant after the clear, above
+	mb.r = mb.r->nx;							// advance to next run buffer
 	if (mb.r->buffer_state == MP_BUFFER_QUEUED) {// only if queued...
-		mb.r->buffer_state = MP_BUFFER_PENDING;  // pend next buffer
+		mb.r->buffer_state = MP_BUFFER_PENDING;	// pend next buffer
 	}
 	if (mb.w == mb.r) cm_cycle_end();			// end the cycle if the queue empties
 	mb.buffers_available++;
@@ -407,7 +412,7 @@ mpBuf_t * mp_get_last_buffer(void)
 
 void mp_clear_buffer(mpBuf_t *bf) 
 {
-	mpBuf_t *nx = bf->nx;	// save pointers
+	mpBuf_t *nx = bf->nx;			// save pointers
 	mpBuf_t *pv = bf->pv;
 	memset(bf, 0, sizeof(mpBuf_t));
 	bf->nx = nx;					// restore pointers
@@ -416,7 +421,7 @@ void mp_clear_buffer(mpBuf_t *bf)
 
 void mp_copy_buffer(mpBuf_t *bf, const mpBuf_t *bp)
 {
-	mpBuf_t *nx = bf->nx;	// save pointers
+	mpBuf_t *nx = bf->nx;			// save pointers
 	mpBuf_t *pv = bf->pv;
  	memcpy(bf, bp, sizeof(mpBuf_t));
 	bf->nx = nx;					// restore pointers
@@ -426,7 +431,7 @@ void mp_copy_buffer(mpBuf_t *bf, const mpBuf_t *bp)
 #ifdef __DEBUG	// currently this routine is only used by debug routines
 uint8_t mp_get_buffer_index(mpBuf_t *bf) 
 {
-	mpBuf_t *b = bf;		// temp buffer pointer
+	mpBuf_t *b = bf;				// temp buffer pointer
 
 	for (uint8_t i=0; i < PLANNER_BUFFER_POOL_SIZE; i++) {
 		if (b->pv > b) {
@@ -434,7 +439,7 @@ uint8_t mp_get_buffer_index(mpBuf_t *bf)
 		}
 		b = b->pv;
 	}
-	return (PLANNER_BUFFER_POOL_SIZE);	// should never happen
+	return(cm_alarm(PLANNER_BUFFER_POOL_SIZE));	// should never happen
 }
 #endif
 
@@ -450,55 +455,55 @@ void mp_dump_plan_buffer_by_index(uint8_t index) { _dump_plan_buffer(&mb.bf[inde
 
 static void _dump_plan_buffer(mpBuf_t *bf)
 {
-	fprintf_P(stderr, (const PROGMEM char *)("***Runtime Buffer[%d] bstate:%d  mtype:%d  mstate:%d  replan:%d\n"),
+	fprintf_P(stderr, PSTR("***Runtime Buffer[%d] bstate:%d  mtype:%d  mstate:%d  replan:%d\n"),
 			_get_buffer_index(bf),
 			bf->buffer_state,
 			bf->move_type,
 			bf->move_state,
 			bf->replannable);
 
-	print_scalar((const PROGMEM char *)("line number:     "), bf->linenum);
-	print_vector((const PROGMEM char *)("position:        "), mm.position, AXES);
-	print_vector((const PROGMEM char *)("target:          "), bf->target, AXES);
-	print_vector((const PROGMEM char *)("unit:            "), bf->unit, AXES);
-	print_scalar((const PROGMEM char *)("jerk:            "), bf->jerk);
-	print_scalar((const PROGMEM char *)("time:            "), bf->time);
-	print_scalar((const PROGMEM char *)("length:          "), bf->length);
-	print_scalar((const PROGMEM char *)("head_length:     "), bf->head_length);
-	print_scalar((const PROGMEM char *)("body_length:     "), bf->body_length);
-	print_scalar((const PROGMEM char *)("tail_length:     "), bf->tail_length);
-	print_scalar((const PROGMEM char *)("entry_velocity:  "), bf->entry_velocity);
-	print_scalar((const PROGMEM char *)("cruise_velocity: "), bf->cruise_velocity);
-	print_scalar((const PROGMEM char *)("exit_velocity:   "), bf->exit_velocity);
-	print_scalar((const PROGMEM char *)("exit_vmax:       "), bf->exit_vmax);
-	print_scalar((const PROGMEM char *)("entry_vmax:      "), bf->entry_vmax);
-	print_scalar((const PROGMEM char *)("cruise_vmax:     "), bf->cruise_vmax);
-	print_scalar((const PROGMEM char *)("delta_vmax:      "), bf->delta_vmax);
-	print_scalar((const PROGMEM char *)("braking_velocity:"), bf->braking_velocity);
+	print_scalar(PSTR("line number:     "), bf->linenum);
+	print_vector(PSTR("position:        "), mm.position, AXES);
+	print_vector(PSTR("target:          "), bf->target, AXES);
+	print_vector(PSTR("unit:            "), bf->unit, AXES);
+	print_scalar(PSTR("jerk:            "), bf->jerk);
+	print_scalar(PSTR("time:            "), bf->time);
+	print_scalar(PSTR("length:          "), bf->length);
+	print_scalar(PSTR("head_length:     "), bf->head_length);
+	print_scalar(PSTR("body_length:     "), bf->body_length);
+	print_scalar(PSTR("tail_length:     "), bf->tail_length);
+	print_scalar(PSTR("entry_velocity:  "), bf->entry_velocity);
+	print_scalar(PSTR("cruise_velocity: "), bf->cruise_velocity);
+	print_scalar(PSTR("exit_velocity:   "), bf->exit_velocity);
+	print_scalar(PSTR("exit_vmax:       "), bf->exit_vmax);
+	print_scalar(PSTR("entry_vmax:      "), bf->entry_vmax);
+	print_scalar(PSTR("cruise_vmax:     "), bf->cruise_vmax);
+	print_scalar(PSTR("delta_vmax:      "), bf->delta_vmax);
+	print_scalar(PSTR("braking_velocity:"), bf->braking_velocity);
 }
 
 void mp_dump_runtime_state(void)
 {
-	fprintf_P(stderr, (const PROGMEM char *)("***Runtime Singleton (mr)\n"));
-	print_scalar((const PROGMEM char *)("line number:       "), mr.linenum);
-	print_vector((const PROGMEM char *)("position:          "), mr.position, AXES);
-	print_vector((const PROGMEM char *)("target:            "), mr.target, AXES);
-	print_scalar((const PROGMEM char *)("length:            "), mr.length);
+	fprintf_P(stderr, PSTR("***Runtime Singleton (mr)\n"));
+	print_scalar(PSTR("line number:       "), mr.linenum);
+	print_vector(PSTR("position:          "), mr.position, AXES);
+	print_vector(PSTR("target:            "), mr.target, AXES);
+	print_scalar(PSTR("length:            "), mr.length);
 
-	print_scalar((const PROGMEM char *)("move_time:         "), mr.move_time);
-//	print_scalar((const PROGMEM char *)("accel_time;        "), mr.accel_time);
-//	print_scalar((const PROGMEM char *)("elapsed_accel_time:"), mr.elapsed_accel_time);
-	print_scalar((const PROGMEM char *)("midpoint_velocity: "), mr.midpoint_velocity);
-//	print_scalar((const PROGMEM char *)("midpoint_accel:    "), mr.midpoint_acceleration);
-//	print_scalar((const PROGMEM char *)("jerk_div2:         "), mr.jerk_div2);
+	print_scalar(PSTR("move_time:         "), mr.move_time);
+//	print_scalar(PSTR("accel_time;        "), mr.accel_time);
+//	print_scalar(PSTR("elapsed_accel_time:"), mr.elapsed_accel_time);
+	print_scalar(PSTR("midpoint_velocity: "), mr.midpoint_velocity);
+//	print_scalar(PSTR("midpoint_accel:    "), mr.midpoint_acceleration);
+//	print_scalar(PSTR("jerk_div2:         "), mr.jerk_div2);
 
-	print_scalar((const PROGMEM char *)("segments:          "), mr.segments);
-	print_scalar((const PROGMEM char *)("segment_count:     "), mr.segment_count);
-	print_scalar((const PROGMEM char *)("segment_move_time: "), mr.segment_move_time);
-//	print_scalar((const PROGMEM char *)("segment_accel_time:"), mr.segment_accel_time);
-	print_scalar((const PROGMEM char *)("microseconds:      "), mr.microseconds);
-	print_scalar((const PROGMEM char *)("segment_length:	  "), mr.segment_length);
-	print_scalar((const PROGMEM char *)("segment_velocity:  "), mr.segment_velocity);
+	print_scalar(PSTR("segments:          "), mr.segments);
+	print_scalar(PSTR("segment_count:     "), mr.segment_count);
+	print_scalar(PSTR("segment_move_time: "), mr.segment_move_time);
+//	print_scalar(PSTR("segment_accel_time:"), mr.segment_accel_time);
+	print_scalar(PSTR("microseconds:      "), mr.microseconds);
+	print_scalar(PSTR("segment_length:	  "), mr.segment_length);
+	print_scalar(PSTR("segment_velocity:  "), mr.segment_velocity);
 }
 #endif // __DEBUG
 
