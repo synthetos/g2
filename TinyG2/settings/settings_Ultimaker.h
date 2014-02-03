9614297318ca562f8533b98593a8fd83f6b9155c
Rob Giseburt <giseburt@gmail.com>
2014-02-03 00:18:10 -0600

    Minor fixes to make it compile.

--- TinyG2/settings/settings_Ultimaker.h
+++ TinyG2/settings/settings_Ultimaker.h
@@ -101,6 +101,7 @@
 #define X_AXIS_MODE 			AXIS_STANDARD		// xam		see canonical_machine.h cmAxisMode for valid values
 #define X_VELOCITY_MAX 			10000 				// xvm		G0 max velocity in mm/min
 #define X_FEEDRATE_MAX 			X_VELOCITY_MAX		// xfr 		G1 max feed rate in mm/min
+#define X_TRAVEL_MIN			0					// xtn		minimum travel - used by soft limits and homing
 #define X_TRAVEL_MAX 			212					// xtm		travel between switches or crashes
 #define X_JERK_MAX 				40000				// xjm		yes, that's "100 billion" mm/(min^3)
 #define X_JUNCTION_DEVIATION 	JUNCTION_DEVIATION	// xjd
@@ -115,6 +116,7 @@
 #define Y_AXIS_MODE 			AXIS_STANDARD
 #define Y_VELOCITY_MAX 			10000
 #define Y_FEEDRATE_MAX 			Y_VELOCITY_MAX
+#define Y_TRAVEL_MIN 			0
 #define Y_TRAVEL_MAX 			190
 #define Y_JERK_MAX 				40000
 #define Y_JUNCTION_DEVIATION	JUNCTION_DEVIATION
@@ -129,6 +131,7 @@
 #define Z_AXIS_MODE				AXIS_STANDARD
 #define Z_VELOCITY_MAX			1000
 #define Z_FEEDRATE_MAX			Z_VELOCITY_MAX
+#define Z_TRAVEL_MIN 			0
 #define Z_TRAVEL_MAX			220
 #define Z_JERK_MAX				50				// 50,000,000
 #define Z_JUNCTION_DEVIATION	JUNCTION_DEVIATION
@@ -143,6 +146,7 @@
 #define A_AXIS_MODE 			AXIS_RADIUS
 #define A_VELOCITY_MAX 			20000 // set to the same speed as X axis
 #define A_FEEDRATE_MAX 			A_VELOCITY_MAX
+#define A_TRAVEL_MIN 			0
 #define A_TRAVEL_MAX 			10
 #define A_JERK_MAX 				10
 #define A_JUNCTION_DEVIATION	JUNCTION_DEVIATION
@@ -158,6 +162,7 @@
 #define B_AXIS_MODE				AXIS_DISABLED
 #define B_VELOCITY_MAX			3600
 #define B_FEEDRATE_MAX			B_VELOCITY_MAX
+#define B_TRAVEL_MIN 			0
 #define B_TRAVEL_MAX			-1
 //#define B_JERK_MAX				20000000
 #define B_JERK_MAX				20
@@ -174,6 +179,7 @@
 #define C_AXIS_MODE				AXIS_DISABLED
 #define C_VELOCITY_MAX			3600
 #define C_FEEDRATE_MAX			C_VELOCITY_MAX
+#define C_TRAVEL_MIN 			0
 #define C_TRAVEL_MAX			-1
 //#define C_JERK_MAX				20000000
 #define C_JERK_MAX				20
