72f208c6f307d182a6f28d10def53eb67f9a3975
Tom <tom@othermachine.co>
2014-03-08 21:02:16 -0800

    Fix heartbeat LED pin number (was erroneously set to M1 Vref pin)

--- TinyG2/hardware.h
+++ TinyG2/hardware.h
@@ -97,7 +97,7 @@ timer_number exec_timer_num  = 5;	// request exec timer in stepper.cpp
 
 // Pin assignments
 
-pin_number indicator_led_pin_num = kLED_USBTXPinNumber;
+pin_number indicator_led_pin_num = kLED_USBRXPinNumber;
 static PWMOutputPin<indicator_led_pin_num> IndicatorLed;
 
 // Init these to input to keep them high-z
