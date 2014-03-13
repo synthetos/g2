d15616fded436b0060798d8a2cfe55dc259fe888
alden.hart <alden08@harts.org>
2014-03-13 07:22:58 -0400

    035.02 Changed settings to PocketNC

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -36,7 +36,7 @@
 #include "MotatePins.h"
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		035.01	// Experiments to trap minimum time segments in planner
+#define TINYG_FIRMWARE_BUILD   		035.02	// Changed settings to PocketNC
 #endif
 #define TINYG_FIRMWARE_VERSION		0.8		// firmware major version
 #define TINYG_HARDWARE_PLATFORM		3		// hardware platform indicator (2 = Native Arduino Due)
