811d515a78e07468ee2cd1e1d3c52673423852d2
alden.hart <alden08@harts.org>
2014-04-11 20:58:51 -0400

    039.10 Merge tinyg code; testing

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -36,7 +36,7 @@
 #include "MotatePins.h"
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		039.09	// Merge tinyg code; ready for test
+#define TINYG_FIRMWARE_BUILD   		039.10	// Merge tinyg code; testing
 #endif
 #define TINYG_FIRMWARE_VERSION		0.8		// firmware major version
 #define TINYG_HARDWARE_PLATFORM		3		// hardware platform indicator (2 = Native Arduino Due)
