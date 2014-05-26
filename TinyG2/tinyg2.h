e32754694010026d4bf261db7dc0766ac8ed1705
alden.hart <alden08@harts.org>
2014-05-26 08:32:21 -0400

    042.01 Merge from rob branch - smoke test passed

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -36,7 +36,7 @@
 #include "MotatePins.h"
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		041.01	// Testing v9f boards
+#define TINYG_FIRMWARE_BUILD   		042.01	// Merge from rob branch - smoke test passed
 #endif
 #define TINYG_FIRMWARE_VERSION		0.8		// firmware major version
 #define TINYG_HARDWARE_PLATFORM		3		// hardware platform indicator (2 = Native Arduino Due)
