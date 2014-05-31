d051a3e70d96a9c1590346e4af8da63838364c6b
alden.hart <alden08@harts.org>
2014-05-31 10:08:02 -0400

    044.03 Changed over to shapeoko profile

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		044.02	// Cleanup
+#define TINYG_FIRMWARE_BUILD   		044.03	// Changed over to shapeoko profile
 #endif
 #define TINYG_FIRMWARE_VERSION		0.8							// firmware major version
 #define TINYG_HARDWARE_PLATFORM		HW_PLATFORM_TINYG_V9		// hardware platform indicator (2 = Native Arduino Due)
