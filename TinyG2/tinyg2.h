fb76769464736d881d367fe685eef6a4e2258ec6
aldenhart <alden08@harts.org>
2014-06-18 11:45:47 -0400

    046.01 feature-tinyg-410-base first commit

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		045.10 // Testing; dither tests
+#define TINYG_FIRMWARE_BUILD   		046.01 // feature-tinyg-410-base first commit 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.8							// firmware major version
 #define TINYG_HARDWARE_PLATFORM		HW_PLATFORM_TINYG_V9		// hardware platform indicator (2 = Native Arduino Due)
