98575aaeaf0e47357bdd58e0d2be3fe0ceafc866
Rob Giseburt <giseburt@gmail.com>
2014-10-03 10:51:46 -0500

    071.04 — Fix for slow-down of really short moves

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		071.03 // Fix for single-endpoint configuration crash
+#define TINYG_FIRMWARE_BUILD   		071.04 // Fix for slow-down of really short moves
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97						// firmware major version
