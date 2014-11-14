6ec3dfd4d42b4b0eee54402a7ae5652754e0c910
Sarah Tappon <sarah.tappon@gmail.com>
2014-11-14 12:17:29 -0800

    fix probe output to match OMC V1 behavior

--- TinyG2/cycle_probing.cpp
+++ TinyG2/cycle_probing.cpp
@@ -290,12 +290,13 @@ static stat_t _probing_finish()
 
 	// If probe was successful the 'e' word == 1, otherwise e == 0 to signal an error
 	printf_P(PSTR("{\"prb\":{\"e\":%i"), (int)cm.probe_state);
-	if (fp_TRUE(pb.flags[AXIS_X])) printf_P(PSTR(",\"x\":%0.3f"), cm.probe_results[AXIS_X]);
-	if (fp_TRUE(pb.flags[AXIS_Y])) printf_P(PSTR(",\"y\":%0.3f"), cm.probe_results[AXIS_Y]);
-	if (fp_TRUE(pb.flags[AXIS_Z])) printf_P(PSTR(",\"z\":%0.3f"), cm.probe_results[AXIS_Z]);
-	if (fp_TRUE(pb.flags[AXIS_A])) printf_P(PSTR(",\"a\":%0.3f"), cm.probe_results[AXIS_A]);
-	if (fp_TRUE(pb.flags[AXIS_B])) printf_P(PSTR(",\"b\":%0.3f"), cm.probe_results[AXIS_B]);
-	if (fp_TRUE(pb.flags[AXIS_C])) printf_P(PSTR(",\"c\":%0.3f"), cm.probe_results[AXIS_C]);
+    // Print the full probe end location along all axes (matches AVR tinyg behavior)
+	printf_P(PSTR(",\"x\":%0.3f"), cm.probe_results[AXIS_X]);
+	printf_P(PSTR(",\"y\":%0.3f"), cm.probe_results[AXIS_Y]);
+	printf_P(PSTR(",\"z\":%0.3f"), cm.probe_results[AXIS_Z]);
+	printf_P(PSTR(",\"a\":%0.3f"), cm.probe_results[AXIS_A]);
+	printf_P(PSTR(",\"b\":%0.3f"), cm.probe_results[AXIS_B]);
+	printf_P(PSTR(",\"c\":%0.3f"), cm.probe_results[AXIS_C]);
 	printf_P(PSTR("}}\n"));
 
 	return (_set_pb_func(_probing_finalize_exit));
