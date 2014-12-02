4b67d28f7632b0217d10e9f95442c409c8374d35
Sarah Tappon <sarah.tappon@gmail.com>
2014-12-02 12:17:04 -0800

    add feedhold-related state machine properties to SRs

--- TinyG2/settings/settings_othermill.h
+++ TinyG2/settings/settings_othermill.h
@@ -74,7 +74,7 @@
 #define MOTOR_POWER_MODE MOTOR_POWER_REDUCED_WHEN_IDLE
 
 #undef  STATUS_REPORT_DEFAULTS
-#define STATUS_REPORT_DEFAULTS  "mpox","mpoy","mpoz","ofsx","ofsy","ofsz","g55x","g55y","g55z","unit","stat","coor","momo","dist","home","mots","plan","line","path","frmo","prbe","safe","estp","spc"
+#define STATUS_REPORT_DEFAULTS  "mpox","mpoy","mpoz","ofsx","ofsy","ofsz","g55x","g55y","g55z","unit","stat","coor","momo","dist","home","mots","plan","line","path","frmo","prbe","safe","estp","spc","hold","macs","cycs"
 
 #undef	COMM_MODE
 #define COMM_MODE					JSON_MODE
