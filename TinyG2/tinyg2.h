4341478aa0b04d7847e20f6c3214eaadc91e62fb
aldenhart <alden08@harts.org>
2015-02-23 12:10:50 -0500

    First commit

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		078.01 // re-arranged mp_aline()
+#define TINYG_FIRMWARE_BUILD   		079.01 // new feature branch for work on input / output
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97						// firmware major version
