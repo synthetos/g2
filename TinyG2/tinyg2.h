4d20cd1fe0dde9ec2e0a16ffae9bc3b29133244c
Rob Giseburt <giseburt@gmail.com>
2015-03-03 19:32:06 -0600

    078.02 // changed the default seetings for gShield

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		078.01 // re-arranged mp_aline()
+#define TINYG_FIRMWARE_BUILD   		078.02 // changed the default seetings for gShield
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97						// firmware major version
