9a8ea0974589926551e25de90eb22fd4c2b873eb
alden.hart <alden08@harts.org>
2015-03-17 19:18:54 -0400

    079.60 fixed bug in cm_end_queue_flush

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		079.59 // cleanup spindle controls and some other code
+#define TINYG_FIRMWARE_BUILD   		079.60 // fixed bug in cm_end_queue_flush
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97						// firmware major version
