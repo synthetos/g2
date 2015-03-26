3215c7a65d8fe56a3f969b320e6c91157d988a36
aldenhart <alden08@harts.org>
2015-03-26 06:32:36 -0400

    minor

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		082.03 // const's and char_t's
+#define TINYG_FIRMWARE_BUILD   		082.04 // removed char_t's
 #endif
 
 #define TINYG_FIRMWARE_VERSION		0.98						// firmware major version
