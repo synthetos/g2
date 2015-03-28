cd2ed4bebaa74ae37f5e38a790225809c0172675
alden.hart <alden08@harts.org>
2015-03-28 07:06:50 -0400

    updated build number

--- TinyG2/tinyg2.h
+++ TinyG2/tinyg2.h
@@ -38,7 +38,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		083.02 // changed segment timing; made time accounting a static function; deleted some old debug code; instrumented some planner assertions
+#define TINYG_FIRMWARE_BUILD   		083.03 // updated panic messages to be more informative
 #endif
 
 #define TINYG_FIRMWARE_VERSION		0.98						// firmware major version
