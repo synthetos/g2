41a97864377c925bfd0faba2a3c7830e7d9a9054
Rob Giseburt <giseburt@gmail.com>
2015-04-08 15:01:40 -0500

    Rearranging the debug from the old Tools to Resources

---
+++ Resources/error.py
@@ -0,0 +1,16 @@
+import re
+import json
+
+codes = {}
+with open('../TinyG2/tinyg2.h') as fp:
+    for line in fp.readlines():
+        match = re.match('\s*#define\s*(\w+)\s*([0-9]+)\s*(?://([\w\s]*))?', line)
+        if match:
+            name, value, description = match.groups()
+            description = description.strip() if description else ''
+            codes[value] = (name, description)
+
+with open('g2_errors.json', 'w') as fp:
+    json.dump(codes, fp, indent=3)
+
+print 'Error codes written to g2_errors.json.'
