1dbd6db02218a775921d95bfd67c59656373c0c6
Rob Giseburt <giseburt@gmail.com>
2015-05-27 11:22:04 -0500

    Added G2v9 and a updated Motate.

---
+++ TinyG2/board/G2v9.mk
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+# This file is part of the Motate project.
+
+ifeq ("$(BOARD)","G2v9i")
+
+    BASE_BOARD=G2v9
+    DEVICE_DEFINES += MOTATE_BOARD="G2v9i" SETTINGS_FILE=${SETTINGS_FILE}
+
+endif
+
+ifeq ("$(BOARD)","G2v9k")
+
+    BASE_BOARD=G2v9
+    DEVICE_DEFINES += MOTATE_BOARD="G2v9k" SETTINGS_FILE=${SETTINGS_FILE}
+
+endif
+
+#### Example config-specific additions:
+
+ifeq ("$(BOARD)","UltimakerTests")
+
+    BASE_BOARD=G2v9
+    SETTINGS_FILE="settings_Ultimaker_Rob_v9h.h"
+    DEVICE_DEFINES += MOTATE_BOARD="G2v9k" SETTINGS_FILE=${SETTINGS_FILE}
+
+endif
+
+ifeq ("$(BOARD)","ShapeokoDualY")
+
+    BASE_BOARD=G2v9
+    SETTINGS_FILE="settings_shapeoko375_dual.h"
+    DEVICE_DEFINES += MOTATE_BOARD="G2v9k" SETTINGS_FILE=${SETTINGS_FILE}
+
+endif
+
+
+##########
+
+ifeq ("$(BASE_BOARD)","G2v9")
+    _BOARD_FOUND = 1
+
+    FIRST_LINK_SOURCES += $(wildcard ${MOTATE_PATH}/Atmel_sam3xa/*.cpp)
+
+    # Set CHIP and export it for GDB to see
+    CHIP = SAM3X8C
+    export CHIP
+    CHIP_LOWERCASE = sam3x8c
+
+    BOARD_PATH = ./board/G2v9
+    DEVICE_INCLUDE_DIRS += $(BOARD_PATH)
+
+    PLATFORM_BASE = ${MOTATE_PATH}/platform/atmel_sam
+
+    include $(PLATFORM_BASE).mk
+endif
--- TinyG2/board/G2v9/G2v9k-pinout.h
+++ TinyG2/board/G2v9/G2v9k-pinout.h
@@ -41,7 +41,7 @@
 #include <MotatePins.h>
 
 namespace Motate {
-    
+
     // v9_3x8c - The SAM3X8C is a 100-pin sister to the SAM3X8E that is on the Due
     // The SAM3X8C is missing port C and D.
 
@@ -88,148 +88,119 @@ namespace Motate {
 
     ***/
 
-    
+
     /*** Pins 100+ are board specific functions.
     * Second (+) SPI or I2C would be here.
     * Special non-kinen devices, LEDs, etc.
     **/
-    
-    
+
+
     // First we define Motate::Pin<> object templates,
     // then we define the pin_number aliases.
-    
-    
+
+
     // All-Fin pins
-    
+
     // Pin 0 - Serial RX - missing!
     // Pin 1 - Serial TX - missing!
-    
+
     _MAKE_MOTATE_PIN(4, A, 'A', 27);	// SPI0_SCKPinNumber
-    _MAKE_MOTATE_SPI_OTHER_PIN(4, A);
-
     _MAKE_MOTATE_PIN(5, A, 'A', 25);	// SPI0_MISOPinNumber
-    _MAKE_MOTATE_SPI_OTHER_PIN(5, A);
-
     _MAKE_MOTATE_PIN(6, A, 'A', 26);	// SPI0_MOSIPinNumber
-    _MAKE_MOTATE_SPI_OTHER_PIN(6, A);
 
     _MAKE_MOTATE_PIN(10, A, 'A', 28);	// Socket1_SPISlaveSelectPinNumber
-    _MAKE_MOTATE_SPI_CS_PIN(10, A, 0);
-
     _MAKE_MOTATE_PIN(12, A, 'A', 23);	// Socket1_StepPinNumber
-
     _MAKE_MOTATE_PIN(13, A, 'A', 6);	// Socket1_DirPinNumber
-    _MAKE_MOTATE_PWM_PIN(13, Motate::Timer<2>, /*Channel:*/ B, /*Peripheral:*/ A, /*Inverted:*/ false);
-
     _MAKE_MOTATE_PIN(14, A, 'A', 22);	// Socket1_EnablePinNumber
-
     _MAKE_MOTATE_PIN(15, A, 'A', 24);	// Socket1_Microstep_0PinNumber
-
     _MAKE_MOTATE_PIN(16, A, 'A', 16);	// Socket1_Microstep_1PinNumber
-
     _MAKE_MOTATE_PIN(17, B, 'B', 16);	// Socket1_Microstep_2PinNumber
-    _MAKE_MOTATE_PWM_PIN(17, Motate::PWMTimer<0>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
-
     _MAKE_MOTATE_PIN(18, B, 'B', 17);	// Socket1_VrefPinNumber
-    _MAKE_MOTATE_PWM_PIN(18, Motate::PWMTimer<1>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
 
     _MAKE_MOTATE_PIN(20, B, 'B', 20);	// Socket2_SPISlaveSelectPinNumber
-    _MAKE_MOTATE_SPI_CS_PIN(20, B, 1);
-
     _MAKE_MOTATE_PIN(22, B, 'B', 0);	// Socket2_StepPinNumber
-
     _MAKE_MOTATE_PIN(23, B, 'B', 2);	// Socket2_DirPinNumber
-
     _MAKE_MOTATE_PIN(24, B, 'B', 1);	// Socket2_EnablePinNumber
-
     _MAKE_MOTATE_PIN(25, A, 'A', 29);	// Socket2_Microstep_0PinNumber
-    _MAKE_MOTATE_SPI_CS_PIN(25, A, 1);
-
     _MAKE_MOTATE_PIN(26, A, 'A', 21);	// Socket2_Microstep_1PinNumber
-    _MAKE_MOTATE_PWM_PIN(26, Motate::PWMTimer<0>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
-
     _MAKE_MOTATE_PIN(27, A, 'A', 4);	// Socket2_Microstep_2PinNumber
-
     _MAKE_MOTATE_PIN(28, B, 'B', 18);	// Socket2_VrefPinNumber
-    _MAKE_MOTATE_PWM_PIN(28, Motate::PWMTimer<2>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
 
     _MAKE_MOTATE_PIN(30, B, 'B', 21);	// Socket3_SPISlaveSelectPinNumber
-    _MAKE_MOTATE_SPI_CS_PIN(30, B, 2);
-
     _MAKE_MOTATE_PIN(32, B, 'B', 6);	// Socket3_StepPinNumber
-
     _MAKE_MOTATE_PIN(33, B, 'B', 8);	// Socket3_DirPinNumber
-
     _MAKE_MOTATE_PIN(34, B, 'B', 7);	// Socket3_EnablePinNumber
-
     _MAKE_MOTATE_PIN(35, B, 'B', 5);	// Socket3_Microstep_0PinNumber
-
     _MAKE_MOTATE_PIN(36, B, 'B', 4);	// Socket3_Microstep_1PinNumber
-
     _MAKE_MOTATE_PIN(37, B, 'B', 3);	// Socket3_Microstep_2PinNumber
-
     _MAKE_MOTATE_PIN(38, B, 'B', 19);	// Socket3_VrefPinNumber
-    _MAKE_MOTATE_PWM_PIN(38, Motate::PWMTimer<3>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
 
     _MAKE_MOTATE_PIN(40, B, 'B', 23);	// Socket4_SPISlaveSelectPinNumber
-    _MAKE_MOTATE_SPI_CS_PIN(40, B, 3);
-
     _MAKE_MOTATE_PIN(42, B, 'B', 14);	// Socket4_StepPinNumber
-    _MAKE_MOTATE_PWM_PIN(42, Motate::PWMTimer<2>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-
     _MAKE_MOTATE_PIN(43, B, 'B', 24);	// Socket4_DirPinNumber
-
     _MAKE_MOTATE_PIN(44, B, 'B', 22);	// Socket4_EnablePinNumber
-
     _MAKE_MOTATE_PIN(45, B, 'B', 11);	// Socket4_Microstep_0PinNumber
-
     _MAKE_MOTATE_PIN(46, B, 'B', 10);	// Socket4_Microstep_1PinNumber
-
     _MAKE_MOTATE_PIN(47, B, 'B', 9);	// Socket4_Microstep_2PinNumber
-
     _MAKE_MOTATE_PIN(48, A, 'A', 2);	// Socket4_VrefPinNumber
-    _MAKE_MOTATE_PWM_PIN(48, Motate::Timer<1>, /*Channel:*/ A, /*Peripheral:*/ A, /*Inverted:*/ false);
 
     _MAKE_MOTATE_PIN(58, A, 'A', 3);	// Socket5_VrefPinNumber
-    _MAKE_MOTATE_PWM_PIN(58, Motate::Timer<1>, /*Channel:*/ B, /*Peripheral:*/ A, /*Inverted:*/ false);
 
     _MAKE_MOTATE_PIN(100, B, 'B', 26);	// XAxis_MinPinNumber
-
     _MAKE_MOTATE_PIN(101, A, 'A', 9);	// XAxis_MaxPinNumber
-    _MAKE_MOTATE_PWM_PIN(101, Motate::PWMTimer<3>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-
     _MAKE_MOTATE_PIN(102, A, 'A', 10);	// YAxis_MinPinNumber
-
     _MAKE_MOTATE_PIN(103, A, 'A', 11);	// YAxis_MaxPinNumber
-
     _MAKE_MOTATE_PIN(104, A, 'A', 12);	// ZAxis_MinPinNumber
-    _MAKE_MOTATE_PWM_PIN(104, Motate::PWMTimer<1>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
-
     _MAKE_MOTATE_PIN(105, A, 'A', 13);	// ZAxis_MaxPinNumber
-    _MAKE_MOTATE_PWM_PIN(105, Motate::PWMTimer<2>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-
     _MAKE_MOTATE_PIN(106, A, 'A', 14);	// AAxis_MinPinNumber
-
     _MAKE_MOTATE_PIN(107, A, 'A', 15);	// AAxis_MaxPinNumber
 
     _MAKE_MOTATE_PIN(112, A, 'A', 5);	// Spindle_EnablePinNumber
-
     _MAKE_MOTATE_PIN(113, A, 'A', 7);	// Spindle_DirPinNumber
-
     _MAKE_MOTATE_PIN(114, A, 'A', 8);	// Spindle_PwmPinNumber
-    _MAKE_MOTATE_PWM_PIN(114, Motate::PWMTimer<0>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-
     _MAKE_MOTATE_PIN(116, A, 'A', 1);	// Coolant_EnablePinNumber
 
     _MAKE_MOTATE_PIN(117, A, 'A', 18);	// LED_USBRXPinNumber
-
     _MAKE_MOTATE_PIN(118, A, 'A', 19);	// LED_USBTXPinNumber
-    _MAKE_MOTATE_PWM_PIN(118, Motate::PWMTimer<1>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
 
     _MAKE_MOTATE_PIN(119, B, 'B', 15);	// SD_CardDetect
-    _MAKE_MOTATE_PWM_PIN(119, Motate::PWMTimer<3>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-
     _MAKE_MOTATE_PIN(120, A, 'A', 17);	// Interlock_In
+
+
+//*** SPI ***
+
+    _MAKE_MOTATE_SPI_SCK_PIN( 'A', 27, /*Peripheral:*/ A);
+    _MAKE_MOTATE_SPI_MISO_PIN('A', 25, /*Peripheral:*/ A);
+    _MAKE_MOTATE_SPI_MOSI_PIN('A', 26, /*Peripheral:*/ A);
+
+    _MAKE_MOTATE_SPI_CS_PIN(  'A', 28, /*Peripheral:*/ A, 0);
+    _MAKE_MOTATE_SPI_CS_PIN(  'A', 29, /*Peripheral:*/ A, 1);
+    _MAKE_MOTATE_SPI_CS_PIN(  'B', 20, /*Peripheral:*/ B, 1);
+    _MAKE_MOTATE_SPI_CS_PIN(  'B', 21, /*Peripheral:*/ B, 2);
+    _MAKE_MOTATE_SPI_CS_PIN(  'B', 23, /*Peripheral:*/ B, 3);
+
+//*** PWM ***
+
+    _MAKE_MOTATE_PWM_PIN('A', 2,  Motate::Timer<1>,    /*Channel:*/ 0, /*Peripheral:*/ A, /*Inverted:*/ false);
+    _MAKE_MOTATE_PWM_PIN('A', 3,  Motate::Timer<1>,    /*Channel:*/ 1, /*Peripheral:*/ A, /*Inverted:*/ false);
+    _MAKE_MOTATE_PWM_PIN('A', 6,  Motate::Timer<2>,    /*Channel:*/ 1, /*Peripheral:*/ A, /*Inverted:*/ false);
+
+    _MAKE_MOTATE_PWM_PIN('A', 8,  Motate::PWMTimer<0>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+    _MAKE_MOTATE_PWM_PIN('A', 21, Motate::PWMTimer<0>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+    _MAKE_MOTATE_PWM_PIN('B', 16, Motate::PWMTimer<0>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+
+    _MAKE_MOTATE_PWM_PIN('A', 12, Motate::PWMTimer<1>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+    _MAKE_MOTATE_PWM_PIN('A', 19, Motate::PWMTimer<1>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+    _MAKE_MOTATE_PWM_PIN('B', 17, Motate::PWMTimer<1>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+
+    _MAKE_MOTATE_PWM_PIN('A', 13, Motate::PWMTimer<2>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+    _MAKE_MOTATE_PWM_PIN('B', 14, Motate::PWMTimer<2>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+    _MAKE_MOTATE_PWM_PIN('B', 18, Motate::PWMTimer<2>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+
+    _MAKE_MOTATE_PWM_PIN('A', 9,  Motate::PWMTimer<3>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+    _MAKE_MOTATE_PWM_PIN('B', 19, Motate::PWMTimer<3>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ false);
+    _MAKE_MOTATE_PWM_PIN('B', 15, Motate::PWMTimer<3>, /*Channel:*/ 0, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
+
 
     //UNASSIGNED, and disconnected:
 //    _MAKE_MOTATE_PIN(, A, 'A', 0);	//
@@ -246,17 +217,17 @@ namespace Motate {
 //
 //    _MAKE_MOTATE_PIN(, A, 'A', 20);	//
 //    _MAKE_MOTATE_PWM_PIN(, Motate::PWMTimer<2>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ false);
-//    
-//    _MAKE_MOTATE_PIN(, B, 'B', 12);	// 
+//
+//    _MAKE_MOTATE_PIN(, B, 'B', 12);	//
 //    _MAKE_MOTATE_PWM_PIN(, Motate::PWMTimer<0>, /*Channel:*/ A, /*Peripheral:*/ B, /*Inverted:*/ true); // INVERTED!
-//    
+//
 //    _MAKE_MOTATE_PIN(, B, 'B', 28);	//
-//    
-//    _MAKE_MOTATE_PIN(, B, 'B', 29);	// 
-//    
-//    _MAKE_MOTATE_PIN(, B, 'B', 30);	// 
-//    
-//    _MAKE_MOTATE_PIN(, B, 'B', 31);	// 
+//
+//    _MAKE_MOTATE_PIN(, B, 'B', 29);	//
+//
+//    _MAKE_MOTATE_PIN(, B, 'B', 30);	//
+//
+//    _MAKE_MOTATE_PIN(, B, 'B', 31);	//
 
 
 } // namespace Motate
