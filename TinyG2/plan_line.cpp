2101762fa65b312587818025c34ba7efa8ee2d60
Rob Giseburt <giseburt@gmail.com>
2015-08-28 21:17:22 -0500

    Commit of just the planner traps. (To be removed later.)

--- TinyG2/plan_line.cpp
+++ TinyG2/plan_line.cpp
@@ -354,7 +354,23 @@ static mpBuf_t *_plan_block(mpBuf_t *bf)
             ASCII_ART("\\");
         }
     }
+    if (bf->entry_velocity > bf->cruise_velocity) {
+        while(1);
+    }
+    if (bf->exit_velocity > bf->cruise_velocity) {
+        while(1);
+    }
     mp_calculate_trapezoid(bf);
+    if (bf->entry_velocity > bf->cruise_velocity) {
+        while(1);
+    }
+    if (bf->exit_velocity > bf->cruise_velocity) {
+        while(1);
+    }
+    if (bf->head_length > 0.0 && bf->head_time < 0.000001) {
+        while(1);
+    }
+
     bf->buffer_state = MP_BUFFER_PLANNED;
     _set_diagnostics(bf);   //+++++ DIAGNOSTIC - need to call a function to get GCC pragmas right
     return (bf_ret);
