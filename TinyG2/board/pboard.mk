5246b8d124e6334180db2159fb32cb1446645d08
Rob Giseburt <giseburt@gmail.com>
2016-01-12 11:40:29 -0600

    pboard-a changes

---
+++ TinyG2/board/pboard.mk
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------
+# This file is part of the Motate project.
+
+# These two Printrbot default BOARD to pBoard.
+
+ifeq ("$(CONFIG)","PrintrbotPlus")
+    ifeq ("$(BOARD)","NONE")
+        BOARD = pboard-a
+    endif
+    SETTINGS_FILE="settings_Printrbot_Plus.h"
+    DEVICE_DEFINES += SETTINGS_FILE=${SETTINGS_FILE}
+endif
+
+ifeq ("$(CONFIG)","PrintrbotSimple")
+    ifeq ("$(BOARD)","NONE")
+        BOARD = pboard-a
+    endif
+    SETTINGS_FILE="settings_Printrbot_Simple.h"
+    DEVICE_DEFINES += SETTINGS_FILE=${SETTINGS_FILE}
+endif
+
+
+ifeq ("$(BOARD)","pboard-a")
+    BASE_BOARD=pboard-a
+    DEVICE_DEFINES += MOTATE_BOARD="pboard-a"
+    DEVICE_DEFINES += MOTATE_CONFIG_HAS_USBSERIAL=1
+endif
+
+ifeq ("$(BASE_BOARD)","pboard-a")
+    _BOARD_FOUND = 1
+
+    FIRST_LINK_SOURCES += $(wildcard ${MOTATE_PATH}/Atmel_sam3xa/*.cpp)
+
+    CHIP = SAM3X8C
+    export CHIP
+    CHIP_LOWERCASE = sam3x8c
+
+    BOARD_PATH = ./board/pboard
+    DEVICE_INCLUDE_DIRS += ${BOARD_PATH}
+
+    PLATFORM_BASE = ${MOTATE_PATH}/platform/atmel_sam
+    include $(PLATFORM_BASE).mk
+endif
