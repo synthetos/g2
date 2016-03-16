0185a8e210ef4022a16deea36aec4bab11096163
Rob Giseburt <giseburt@gmail.com>
2016-03-16 13:14:41 -0500

    Update Tools/error.py

--- Tools/error.py
+++ Tools/error.py
@@ -2,13 +2,19 @@ import re
 import json
 
 codes = {}
-with open('../TinyG2/tinyg2.h') as fp:
+with open('../TinyG2/error.h') as fp:
     for line in fp.readlines():
-        match = re.match('\s*#define\s*(\w+)\s*([0-9]+)\s*(?://([\w\s\'0-9]*))?', line)
+        match = re.match(r'\s*#define\s*(\w+)\s*([0-9]+)\s*(?://([\w\s\'0-9]*))?', line)
         if match:
             name, value, description = match.groups()
             description = description.strip() if description else ''
             codes[value] = (name, description)
+        else:
+            match = re.match(r'\s*static const char stat_\([0-9]+\)\[\] PROGMEM = \"(\\"|[^"])+\"', line)
+            if match:
+                value, description = match.groups()
+                description = description.strip() if description else ''
+                codes[value][1] = description
 
 with open('g2_errors.json', 'w') as fp:
     json.dump(codes, fp, indent=3)
