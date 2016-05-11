b822dfe4f81c74b9f7dd10ec2c8029a7b9e642e6
Rob Giseburt <giseburt@gmail.com>
2016-05-11 12:59:27 -0500

    099.02 // PB settings adjustments

--- TinyG2/tinyg2_info.h
+++ TinyG2/tinyg2_info.h
@@ -21,7 +21,7 @@
 #ifndef INFO_H_ONCE
 #define INFO_H_ONCE
 
-#define TINYG_FIRMWARE_BUILD            099.01 // linemode PB adjustments
+#define TINYG_FIRMWARE_BUILD            099.02 // PB settings adjustments
 #ifdef GIT_VERSION
 #define TINYG_FIRMWARE_BUILD_STRING     GIT_VERSION
 #else
