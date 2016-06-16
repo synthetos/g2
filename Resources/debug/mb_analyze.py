c75b47b77d2f2ed21339a02f396524a9ab143a4f
Rob Giseburt <giseburt@gmail.com>
2016-06-16 13:21:34 -0500

    fixes to mb_analyze.py (dump_mb)

--- Resources/debug/mb_analyze.py
+++ Resources/debug/mb_analyze.py
@@ -20,62 +20,66 @@ def load_pool(filename):
         lines = mb.readlines()
     pool = {}
     for line in lines:
-        buf = re.search('\(mpBuf_t\s\*\)\s(0x[0-9a-fA-F]+)', line)
-        if buf:
-            current_buffer = int(buf.groups()[0], 16)
-            pool[TUMBLER[tumbler_pos]] = current_buffer
-            tumbler_pos += 1
-
-        buf = re.search('\(mpBuffer\s\*\)\s(0x[0-9a-fA-F]+)', line)
-        if buf:
-            current_buffer = int(buf.groups()[0], 16)
-            pool[current_buffer] = {'move_time': 0}
-            if current_buffer == pool['r']:
-                pool[current_buffer]['is_r'] = True
-            if current_buffer == pool['w']:
-                pool[current_buffer]['is_w'] = True
-            if current_buffer == pool['p']:
-                pool[current_buffer]['is_p'] = True
-            if current_buffer == pool['c']:
-                pool[current_buffer]['is_c'] = True
+        if tumbler_pos < len(TUMBLER):
+            buf = re.search('\(mpBuf_t\s\*\)\s(0x[0-9a-fA-F]+)', line)
+            if buf:
+                current_buffer = int(buf.groups()[0], 16)
+                pool[TUMBLER[tumbler_pos]] = current_buffer
+                # print "TUMBLER[tumbler_pos]: %s=%i" % (TUMBLER[tumbler_pos], current_buffer)
+                tumbler_pos += 1
         else:
-            match = re.search('([a-zA-Z_]+)\s=\s((?:0x[0-9a-fA-F]+|[0-9.]+(?:e[-+][0-9]+)?|[A-Z_]+)|true|false)', line)
-            if match:
-                key, val = match.groups()
-                if key == 'pv':
-                    pv = int(val, 16)
-                    pool[current_buffer]['pv'] = pv
-                elif key == 'nx':
-                    nv = int(val, 16)
-                    pool[current_buffer]['nx'] = nv
-                elif key == 'buffer_state':
-                    pool[current_buffer]['buffer_state'] = val
-                elif key == 'jerk':
-                    pool[current_buffer]['jerk'] = val
-                elif key == 'linenum':
-                    pool[current_buffer]['linenum'] = val
-                elif key == 'buffer_number':
-                    pool[current_buffer]['buffer_number'] = val
-                elif key == 'junction_vmax':
-                    pool[current_buffer]['junction_vmax'] = float(val)
-                elif key == 'cruise_velocity':
-                    pool[current_buffer]['cruise_velocity'] = float(val)
-                elif key == 'cruise_vmax':
-                    pool[current_buffer]['cruise_vmax'] = float(val)
-                elif key == 'exit_vmax':
-                    pool[current_buffer]['exit_vmax'] = float(val)
-                elif key == 'exit_velocity':
-                    pool[current_buffer]['exit_velocity'] = float(val)
-                elif key == 'length':
-                    pool[current_buffer]['length'] = float(val)
-                elif key == 'move_time':
-                    pool[current_buffer]['move_time'] = float(val)
-                elif key == 'plannable':
-                    pool[current_buffer]['plannable'] = val
-                elif key == 'hint':
-                    pool[current_buffer]['hint'] = val
-                elif key == 'iterations':
-                    pool[current_buffer]['iterations'] = val
+            buf = re.search('\(mpBuf_t\s\*\)\s(0x[0-9a-fA-F]+)', line)
+            if buf:
+                current_buffer = int(buf.groups()[0], 16)
+                # print "setup current_buffer: %s" % current_buffer
+                pool[current_buffer] = {'move_time': 0}
+                if current_buffer == pool['r']:
+                    pool[current_buffer]['is_r'] = True
+                if current_buffer == pool['w']:
+                    pool[current_buffer]['is_w'] = True
+                if current_buffer == pool['p']:
+                    pool[current_buffer]['is_p'] = True
+                if current_buffer == pool['c']:
+                    pool[current_buffer]['is_c'] = True
+            else:
+                match = re.search('([a-zA-Z_]+)\s=\s((?:0x[0-9a-fA-F]+|[0-9.]+(?:e[-+][0-9]+)?|[A-Z_]+)|true|false)', line)
+                if match:
+                    # print "current_buffer: %s" % current_buffer
+                    key, val = match.groups()
+                    if key == 'pv':
+                        pv = int(val, 16)
+                        pool[current_buffer]['pv'] = pv
+                    elif key == 'nx':
+                        nv = int(val, 16)
+                        pool[current_buffer]['nx'] = nv
+                    elif key == 'buffer_state':
+                        pool[current_buffer]['buffer_state'] = val
+                    elif key == 'jerk':
+                        pool[current_buffer]['jerk'] = val
+                    elif key == 'linenum':
+                        pool[current_buffer]['linenum'] = val
+                    elif key == 'buffer_number':
+                        pool[current_buffer]['buffer_number'] = val
+                    elif key == 'junction_vmax':
+                        pool[current_buffer]['junction_vmax'] = float(val)
+                    elif key == 'cruise_velocity':
+                        pool[current_buffer]['cruise_velocity'] = float(val)
+                    elif key == 'cruise_vmax':
+                        pool[current_buffer]['cruise_vmax'] = float(val)
+                    elif key == 'exit_vmax':
+                        pool[current_buffer]['exit_vmax'] = float(val)
+                    elif key == 'exit_velocity':
+                        pool[current_buffer]['exit_velocity'] = float(val)
+                    elif key == 'length':
+                        pool[current_buffer]['length'] = float(val)
+                    elif key == 'move_time':
+                        pool[current_buffer]['move_time'] = float(val)
+                    elif key == 'plannable':
+                        pool[current_buffer]['plannable'] = val
+                    elif key == 'hint':
+                        pool[current_buffer]['hint'] = val
+                    elif key == 'iterations':
+                        pool[current_buffer]['iterations'] = val
     return pool
 
 def check_integrity(pool):
