091f14d3fd89b00b14178acdf26f6cc78799575c
Rob Giseburt <giseburt@gmail.com>
2016-06-22 20:06:22 -0500

    PB Simple/Play settings update

--- TinyG2/settings/settings_Printrbot_Play.h
+++ TinyG2/settings/settings_Printrbot_Play.h
@@ -102,48 +102,48 @@
 
 #define MOTOR_POWER_MODE            MOTOR_POWERED_IN_CYCLE  // default motor power mode (see cmMotorPowerMode in stepper.h)
 // 80 steps/mm at 1/16 microstepping = 40 mm/rev
-#define M5_MOTOR_MAP                AXIS_X                  // 1ma
-#define M5_STEP_ANGLE               1.8                     // 1sa
-#define M5_TRAVEL_PER_REV           40.64                      // 1tr
-#define M5_MICROSTEPS               32                      // 1mi		1,2,4,8,16,32
-#define M5_POLARITY                 1                       // 1po		0=normal, 1=reversed
-#define M5_POWER_MODE               MOTOR_POWER_MODE        // 1pm		standard
-#define M5_POWER_LEVEL              0.6                    // 1mp
+#define M1_MOTOR_MAP                AXIS_X                  // 1ma
+#define M1_STEP_ANGLE               1.8                     // 1sa
+#define M1_TRAVEL_PER_REV           40.64                      // 1tr
+#define M1_MICROSTEPS               32                      // 1mi		1,2,4,8,16,32
+#define M1_POLARITY                 1                       // 1po		0=normal, 1=reversed
+#define M1_POWER_MODE               MOTOR_POWER_MODE        // 1pm		standard
+#define M1_POWER_LEVEL              0.6                    // 1mp
 
 // 80 steps/mm at 1/16 microstepping = 40 mm/rev
-#define M4_MOTOR_MAP                AXIS_Y
-#define M4_STEP_ANGLE               1.8
-#define M4_TRAVEL_PER_REV           40.64
-#define M4_MICROSTEPS               32
-#define M4_POLARITY                 1
-#define M4_POWER_MODE               MOTOR_POWER_MODE
-#define M4_POWER_LEVEL              0.6
-
-#define M3_MOTOR_MAP                AXIS_Z
+#define M3_MOTOR_MAP                AXIS_Y
 #define M3_STEP_ANGLE               1.8
-#define M3_TRAVEL_PER_REV           1.5875
+#define M3_TRAVEL_PER_REV           40.64
 #define M3_MICROSTEPS               32
-#define M3_POLARITY                 1
+#define M3_POLARITY                 0
 #define M3_POWER_MODE               MOTOR_POWER_MODE
 #define M3_POWER_LEVEL              0.6
 
-// 96 steps/mm at 1/16 microstepping = 33.3333 mm/rev
-#define M2_MOTOR_MAP                AXIS_A
+#define M2_MOTOR_MAP                AXIS_Z
 #define M2_STEP_ANGLE               1.8
-#define M2_TRAVEL_PER_REV           360			// degrees moved per motor rev
+#define M2_TRAVEL_PER_REV           1.5875
 #define M2_MICROSTEPS               32
-#define M2_POLARITY                 0
+#define M2_POLARITY                 1
 #define M2_POWER_MODE               MOTOR_POWER_MODE
 #define M2_POWER_LEVEL              0.6
 
 // 96 steps/mm at 1/16 microstepping = 33.3333 mm/rev
-#define M1_MOTOR_MAP                AXIS_B
-#define M1_STEP_ANGLE               1.8
-#define M1_TRAVEL_PER_REV           360			// degrees moved per motor rev
-#define M1_MICROSTEPS               32
-#define M1_POLARITY                 0
-#define M1_POWER_MODE               MOTOR_POWER_MODE
-#define M1_POWER_LEVEL              0.35
+#define M4_MOTOR_MAP                AXIS_A
+#define M4_STEP_ANGLE               1.8
+#define M4_TRAVEL_PER_REV           360			// degrees moved per motor rev
+#define M4_MICROSTEPS               32
+#define M4_POLARITY                 0
+#define M4_POWER_MODE               MOTOR_POWER_MODE
+#define M4_POWER_LEVEL              0.6
+
+// 96 steps/mm at 1/16 microstepping = 33.3333 mm/rev
+#define M5_MOTOR_MAP                AXIS_B
+#define M5_STEP_ANGLE               1.8
+#define M5_TRAVEL_PER_REV           360			// degrees moved per motor rev
+#define M5_MICROSTEPS               32
+#define M5_POLARITY                 0
+#define M5_POWER_MODE               MOTOR_POWER_MODE
+#define M5_POWER_LEVEL              0.35
 
 // *** axis settings **********************************************************************************
 
@@ -200,7 +200,7 @@
  *
  * It will return s= as the value you want to enter.
  *
- * If the value is over 1 million, the code will divide it by 1 million, 
+ * If the value is over 1 million, the code will divide it by 1 million,
  * so you have to pre-multiply it by 1000000.0. (The value is in millions, btw.)
  *
  * Note that you need these to be floating point values, so always have a .0 at the end!
@@ -244,7 +244,7 @@
 
 
 //*** Input / output settings ***
-/*  
+/*
     IO_MODE_DISABLED
     IO_ACTIVE_LOW    aka NORMALLY_OPEN
     IO_ACTIVE_HIGH   aka NORMALLY_CLOSED
