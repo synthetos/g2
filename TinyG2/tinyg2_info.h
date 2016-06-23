a6f98fcf776bca7c574b0c6a0a77d825bc13130c
Rob Giseburt <giseburt@gmail.com>
2016-06-23 13:00:34 -0500

    100.04 // set zzb to zero for PrinterBots

--- TinyG2/tinyg2_info.h
+++ TinyG2/tinyg2_info.h
@@ -21,7 +21,7 @@
 #ifndef INFO_H_ONCE
 #define INFO_H_ONCE
 
-#define TINYG_FIRMWARE_BUILD            100.03 // tuned power settings for PrinterBots
+#define TINYG_FIRMWARE_BUILD            100.04 // set zzb to zero for PrinterBots
 #ifdef GIT_VERSION
 #define TINYG_FIRMWARE_BUILD_STRING     GIT_VERSION
 #else
