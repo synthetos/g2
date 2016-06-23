e304018e5d9d9712784394b4da669bfb80686618
Rob Giseburt <giseburt@gmail.com>
2016-06-22 20:44:37 -0500

    100.00 // release candidate 1

--- TinyG2/tinyg2_info.h
+++ TinyG2/tinyg2_info.h
@@ -21,7 +21,7 @@
 #ifndef INFO_H_ONCE
 #define INFO_H_ONCE
 
-#define TINYG_FIRMWARE_BUILD            099.05 // Fixes to debouncing
+#define TINYG_FIRMWARE_BUILD            100.00 // release candidate 1
 #ifdef GIT_VERSION
 #define TINYG_FIRMWARE_BUILD_STRING     GIT_VERSION
 #else
