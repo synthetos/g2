c0de485705a4283c984ee4abc42563ff2a7ae340
Rob Giseburt <giseburt@gmail.com>
2016-09-02 16:57:08 -0500

    100.08  // Added _leds to drive the neopixels

--- g2core/g2core_info.h
+++ g2core/g2core_info.h
@@ -21,7 +21,7 @@
 #ifndef G2CORE_INFO_H_ONCE
 #define G2CORE_INFO_H_ONCE
 
-#define G2CORE_FIRMWARE_BUILD			100.07  // Sticky EJ mode
+#define G2CORE_FIRMWARE_BUILD			100.08  // Added _leds to drive the neopixels
 #ifdef GIT_VERSION
 #define G2CORE_FIRMWARE_BUILD_STRING	GIT_VERSION
 #else
