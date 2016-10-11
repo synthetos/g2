ee8db17b88db56abe02557a0937c99ef10d2c92d
zerodamage <cma@et.aau.dk>
2016-10-11 14:28:10 +0200

    Tabs

--- g2core/settings/settings_ISEL.h
+++ g2core/settings/settings_ISEL.h
@@ -87,7 +87,7 @@
 
 #define M1_MOTOR_MAP                AXIS_X                  // 1ma
 #define M1_STEP_ANGLE               1.8                     // 1sa
-#define M1_TRAVEL_PER_REV           5                   // 1tr
+#define M1_TRAVEL_PER_REV           5						// 1tr
 #define M1_MICROSTEPS               8                       // 1mi  1,2,4,8,16,32
 #define M1_POLARITY                 0                       // 1po  0=normal, 1=reversed
 #define M1_POWER_MODE               MOTOR_POWER_MODE        // 1pm  TRUE=low power idle enabled
