dc2210e9e30155c4b965dc524ec07a8a67046eeb
Rob Giseburt <giseburt@gmail.com>
2017-01-05 16:02:52 -0600

    100.18  // fix startup alarm state

--- g2core/g2core_info.h
+++ g2core/g2core_info.h
@@ -21,7 +21,7 @@
 #ifndef G2CORE_INFO_H_ONCE
 #define G2CORE_INFO_H_ONCE
 
-#define G2CORE_FIRMWARE_BUILD			100.17  // Added tool offsets (PR #211) 
+#define G2CORE_FIRMWARE_BUILD			100.18  // fix startup alarm state 
 #ifdef GIT_VERSION
 #define G2CORE_FIRMWARE_BUILD_STRING	GIT_VERSION
 #else
