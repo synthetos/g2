8df3aeada66345b78315ec3986c3c5754bdffb06
Rob Giseburt <giseburt@gmail.com>
2017-02-23 10:50:11 -0600

    100.21  // Merged dev-209b-random-pauses to compelte USB changes (only fix travis-ci from 100.20)

--- g2core/g2core_info.h
+++ g2core/g2core_info.h
@@ -21,7 +21,7 @@
 #ifndef G2CORE_INFO_H_ONCE
 #define G2CORE_INFO_H_ONCE
 
-#define G2CORE_FIRMWARE_BUILD			100.20  // Merged dev-209b-random-pauses to compelte USB changes
+#define G2CORE_FIRMWARE_BUILD			100.21  // Merged dev-209b-random-pauses to compelte USB changes (only fix travis-ci from 100.20)
 #ifdef GIT_VERSION
 #define G2CORE_FIRMWARE_BUILD_STRING	GIT_VERSION
 #else
