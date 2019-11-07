650322b08887523111ed5095906a952a8fcb77eb
Rob Giseburt <giseburt@gmail.com>
2019-11-07 12:43:09 -0500

    Catch edge case: FEEDHOLD_SYNC with no buffers

--- g2core/planner.cpp
+++ g2core/planner.cpp
@@ -593,6 +593,10 @@ bool mp_is_phat_city_time()
 
 stat_t mp_planner_callback()
 {
+    // Edgge case: If there's no runnable buffer, FEEDHOLD_SYNC will never exit
+    if (cm->hold_state == FEEDHOLD_SYNC && !mp_has_runnable_buffer(&mp1)) {
+        cm->hold_state = FEEDHOLD_MOTION_STOPPED;
+    }
     // Test if the planner has transitioned to an IDLE state
     if ((mp_get_planner_buffers(mp) == mp->q.queue_size) &&     // detect and set IDLE state
         (cm->motion_state == MOTION_STOP) && (cm->hold_state == FEEDHOLD_OFF)) {
