84dfa57466cd23a10736ec1ca7c6c808d2e0d9cb
Matt Long <mhlong10@gmail.com>
2020-01-18 14:25:18 -0500

    Adding VCarve post processors

---
+++ Resources/Post-Processors/VCarve/G2Core_WrapY2A_mm.pp
@@ -0,0 +1,156 @@
++================================================
++                                                
++ Vectric machine output configuration file for
++ g2core.
++                                                
++================================================
++                                                
++ History                                        
++                                                  
++ Who      When       What                         
++ ======== ========== ===========================
++ MattL    05/07/2019 Written from grbl_mm.pp
++                     added descriptive header.
++					  Removed arc moves since not
++					  support on rotary axis.
++ ======== ========== ===========================
+
+POST_NAME = "G2Core WrapY2A (mm) (*.gcode)"
+ 
+FILE_EXTENSION = "gcode"
+ 
+UNITS = "MM"
+
+ROTARY_WRAP_Y = "-A"
+
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+ 
+LINE_ENDING = "[13][10]"
+ 
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+ 
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+ 
++================================================
++                                                
++    Formatting for variables                     
++                                                
++================================================
+ 
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR POWER = [P|C|S|1.0|10.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.3]
+VAR Y_POSITION = [Y|C|Y|1.3]
+VAR Z_POSITION = [Z|C|Z|1.3]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+VAR WRAP_DIAMETER = [WRAP_DIA|A||1.3]
+ 
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+ 
++---------------------------------------------------
++  Commands output at the start of the file
++---------------------------------------------------
+ 
+begin HEADER
+ 
+"( [TP_FILENAME] )"
+"( File created: [DATE] - [TIME])"
+"( for g2core from Vectric )"
+"( Material Size)"
+"( X= [XLENGTH], Z= [ZLENGTH])"
+"( Diameter = [WRAP_DIA] mm)"  
+"( Y Values are wrapped around the X axis )"
+"( Y Values are output as A )"
+"([FILE_NOTES])"
+"(Tools used in this file: )"
+"([TOOLS_USED])"
+"T1"
+"G17"
+"G21"
+"G90"
+"G0[ZH]"
+"G0[XH][YH]"
+
+ 
++---------------------------------------------------
++  Command output after the header to switch spindle on
++---------------------------------------------------
+ 
+begin SPINDLE_ON
+
+"[S]M3"
+
+
++---------------------------------------------------
++  Commands output for rapid moves 
++---------------------------------------------------
+ 
+begin RAPID_MOVE
+ 
+"G0[X][Y][Z]"
+ 
+ 
++---------------------------------------------------
++  Commands output for the first feed rate move
++---------------------------------------------------
+ 
+begin FIRST_FEED_MOVE
+ 
+"G1[X][Y][Z][P][F]"
+ 
+ 
++---------------------------------------------------
++  Commands output for feed rate moves
++---------------------------------------------------
+ 
+begin FEED_MOVE
+ 
+"G1[X][Y][Z][P]"
+ 
++---------------------------------------------------
++  Commands output when the jet is turned on
++---------------------------------------------------
+
+begin JET_TOOL_ON
+
+"M4[P]"
+
++---------------------------------------------------
++  Commands output when the jet is turned off
++---------------------------------------------------
+
+begin JET_TOOL_OFF
+
+"M5"
+
++---------------------------------------------------
++  Commands output when the jet power is changed
++---------------------------------------------------
+
+begin JET_TOOL_POWER
+"[P]"
+ 
++---------------------------------------------------
++  Commands output at the end of the file
++---------------------------------------------------
+ 
+begin FOOTER
+
+"M5"
+"G0[ZH]"
+"G0[XH][YH]"
+"M2"
