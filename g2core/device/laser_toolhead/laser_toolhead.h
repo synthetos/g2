fbc7aff0b6c30ea8770b6c24bae06d0e6a66090e
Rob Giseburt <giseburt@gmail.com>
2020-01-22 15:45:04 -0600

    More work on LaserToolhead (not finished, but working)

--- g2core/device/laser_toolhead/laser_toolhead.h
+++ g2core/device/laser_toolhead/laser_toolhead.h
@@ -29,11 +29,12 @@
 #ifndef LASER_TOOLHEAD_H_ONCE
 #define LASER_TOOLHEAD_H_ONCE
 
+template <typename KinematicsParent, Motate::pin_number fire_num>
 class LaserTool; // Need to forward declare since stepper.h may (eventually) include this file
 
+#include "util.h"
 #include "spindle.h"
 #include "stepper.h" // for Stepper and st_request_load_move
-#include "util.h" // for fp_NE
 #include "safety_manager.h" // for safety_manager
 
 /* A few notes:
@@ -41,14 +42,15 @@ class LaserTool; // Need to forward declare since stepper.h may (eventually) inc
  * This module drives a laser output based on the spindle controls.
  *
  * Laser ON/OFF (NOT fire, just "is active") is on the `enable_output` pin,
- * and actual fire/pulse is on the `pwm_output` pin.
+ * and actual fire/pulse is on the `fire` pin.
  *
  */
 
 
 // class declaration
 // note implementation is after
-class LaserTool : public ToolHead, virtual public Stepper {
+template <typename KinematicsParent, Motate::pin_number fire_num>
+class LaserTool : public ToolHead, public Stepper, public KinematicsParent {
     spDirection direction;        // direction
     float speed;                  // S in RPM
     // float speed_actual;           // actual speed (during speed ramping)
@@ -58,21 +60,33 @@ class LaserTool : public ToolHead, virtual public Stepper {
 
     bool paused;                  // true if paused, false is not
 
-    uint8_t pwm_output_num;
-    gpioDigitalOutput *pwm_output = nullptr;
+    OutputPin<fire_num> fire;
 
     uint8_t enable_output_num;
     gpioDigitalOutput *enable_output = nullptr;
 
     // For "Stepper" enabled control
     bool enabled = false;
+    uint8_t laser_motor;
+
+    float laser_step_position;
+    int16_t pulse_tick_counter = 0;
+    int16_t ticks_per_pulse;
+    int16_t next_ticks_per_pulse;
+    int16_t pulse_duration_us;
+
+    float min_s;
+    float max_s;
+    float min_ppm;
+    float max_ppm;
 
     void complete_change();
 
    public:
+
 // ToolHead functions ----
     // constructor - provide it with the default output pins - 0 means no pin
-    LaserTool(const uint8_t pwm_pin_number, const uint8_t enable_pin_number);
+    LaserTool(const uint8_t enable_pin_number, const uint8_t laser_motor);
 
     // ToolHead overrides
     void init() override;
@@ -99,7 +113,7 @@ class LaserTool : public ToolHead, virtual public Stepper {
 
     bool is_on() override;  // return if the current direction is anything but OFF, **even if paused**
 
-    void set_pwm_output(const uint8_t pwm_pin_number) override;
+    void set_pwm_output(const uint8_t) override;
     void set_enable_output(const uint8_t enable_pin_number) override;
 
     void set_frequency(float new_frequency)override;
@@ -144,6 +158,318 @@ class LaserTool : public ToolHead, virtual public Stepper {
     void stepEnd() override;
     void setDirection(uint8_t new_direction) override;
     void setPowerLevels(float active_pl, float idle_pl) override;
+
+// Kinematics functions ----
+    uint8_t laser_motor_axis = 4;
+    void configure(const float steps_per_unit[MOTORS], const int8_t motor_map[MOTORS]) override;
+    void inverse_kinematics(const GCodeState_t &gm, const float target[AXES], const float position[AXES], const float start_velocity, const float end_velocity, const float segment_time, float steps[MOTORS]) override;
+
+// Laser-specific configuration commands
+    void set_pulse_duration_us(int16_t new_pulse_duration_us);
+    int16_t get_pulse_duration_us();
+
+    float get_min_s();
+    void set_min_s(float new_min_s);
+
+    float get_max_s();
+    void set_max_s(float new_max_s);
+
+    float get_min_ppm();
+    void set_min_ppm(float new_min_ppm);
+
+    float get_max_ppm();
+    void set_max_ppm(float new_max_ppm);
 };
+
+// IMPLEMENTATION
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+LaserTool<KinematicsParent,fire_num>::LaserTool(const uint8_t enable_pin_number, const uint8_t laser_motor_number)
+    : ToolHead{},
+      Stepper{},
+      KinematicsParent{},
+      fire{kStartLow}, // fire_polarity==IO_ACTIVE_LOW?kStartHigh:kStartLow
+      enable_output_num{enable_pin_number},
+      laser_motor{laser_motor_number} {}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::init()
+{
+    // TODO - ensure outputs are within range
+    set_enable_output(enable_output_num);
+    fire.clear();
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::pause() {
+    if (paused) {
+        return;
+    }
+
+    paused = true;
+    fire.clear();
+    this->complete_change();
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::resume() {
+    if (!paused) {
+        return;
+    }
+
+    paused = false;
+    this->complete_change();
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+bool LaserTool<KinematicsParent,fire_num>::ready_to_resume() { return paused && safety_manager->ok_to_spindle(); }
+// template <typename KinematicsParent, Motate::pin_number fire_num>
+// bool LaserTool<KinematicsParent,fire_num>::busy() {
+//     // return true when not paused, on, and ramping up to speed
+//     if (paused || (direction == SPINDLE_OFF)) {
+//         return false;
+//     }
+//     return true;
+// }
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_speed() { return speed; }
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+spDirection LaserTool<KinematicsParent,fire_num>::get_direction() { return direction; }
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::stop() {
+    paused = false;
+    speed = 0;
+    direction = SPINDLE_OFF;
+
+    this->complete_change();
+}
+
+// called from a command that was queued when the default set_speed and set_direction returned true
+// ALSO called from the loader right before a move
+// we are handed the gcode model to use
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::engage(const GCodeState_t &gm) {
+    if ((direction == gm.spindle_direction) && fp_EQ(speed, gm.spindle_speed)) {
+        // nothing changed
+        return;
+    }
+
+    // // special handling for reversals - we set the speed to zero and ramp up
+    // if ((gm.spindle_direction != direction) && (direction != SPINDLE_OFF) && (gm.spindle_direction != SPINDLE_OFF)) {
+    //     speed_actual = 0;
+    // }
+
+    speed = gm.spindle_speed;
+    direction = gm.spindle_direction;
+
+    // handle the rest
+    this->complete_change();
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+bool LaserTool<KinematicsParent,fire_num>::is_on() { return (direction != SPINDLE_OFF); }
+
+// LaserTool-specific functions
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_pwm_output(const uint8_t) {
+    // sometimes I whatever
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_enable_output(const uint8_t enable_pin_number) {
+    if (enable_pin_number == 0) {
+        enable_output = nullptr;
+    } else {
+        enable_output = d_out[enable_pin_number - 1];
+        enable_output->setEnabled(IO_ENABLED);
+        // set the polarity on the output -- not here
+    }
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_frequency(float new_frequency)
+{
+    // use to set pulse width
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_frequency()
+{
+    return 0.0;
+}
+
+// Stepper functons
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::_enableImpl() {
+    ticks_per_pulse =  next_ticks_per_pulse;
+    enabled = true;
+};
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::_disableImpl() {
+    enabled = false;
+};
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::stepStart() {
+    if (!enabled) return;
+    fire.set();
+    pulse_tick_counter = ticks_per_pulse;
+};
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::stepEnd() {
+    if (pulse_tick_counter <= 0) {
+        return;
+    }
+    if (--pulse_tick_counter == 0) {
+        fire.clear();
+    }
+};
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::setDirection(uint8_t new_direction) {
+};
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::setPowerLevels(float active_pl, float idle_pl) {
+    ; // ignore this
+};
+
+
+// Private functions
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::complete_change() {
+    // if the spindle is not on (or paused), make sure we stop it
+    if (paused || direction == SPINDLE_OFF) {
+        if (enable_output != nullptr) {
+            enable_output->setValue(false);
+        }
+
+        return;
+    } else if (direction == SPINDLE_CW) {
+        if (enable_output != nullptr) {
+            enable_output->setValue(true);
+        }
+    } else if (direction == SPINDLE_CCW) {
+        if (enable_output != nullptr) {
+            enable_output->setValue(true);
+        }
+    }
+}
+
+
+// Kinematics functions ----
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::configure(const float new_steps_per_unit[MOTORS], const int8_t new_motor_map[MOTORS]) {
+    int8_t adjusted_motor_map[MOTORS];
+    laser_motor_axis = new_motor_map[laser_motor];
+    for (uint8_t motor = 0; motor < MOTORS; motor++) {
+        if (motor == laser_motor) {
+            adjusted_motor_map[motor] = -1; // keep the parent kinematics from computing this motor
+            continue;
+        }
+        adjusted_motor_map[motor] = new_motor_map[motor];
+    }
+    KinematicsParent::configure(new_steps_per_unit, adjusted_motor_map);
+}
+
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_pulse_duration_us(int16_t new_pulse_duration_us) {
+    pulse_duration_us = new_pulse_duration_us;
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+int16_t LaserTool<KinematicsParent,fire_num>::get_pulse_duration_us() {
+    return pulse_duration_us;
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_min_s() {
+    return min_s;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_min_s(float new_min_s) {
+    min_s = new_min_s;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_max_s() {
+    return max_s;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_max_s(float new_max_s) {
+    max_s = new_max_s;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_min_ppm() {
+    return min_ppm;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_min_ppm(float new_min_ppm) {
+    min_ppm = new_min_ppm;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+float LaserTool<KinematicsParent,fire_num>::get_max_ppm() {
+    return max_ppm;
+}
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::set_max_ppm(float new_max_ppm) {
+    max_ppm = new_max_ppm;
+}
+
+template <typename KinematicsParent, Motate::pin_number fire_num>
+void LaserTool<KinematicsParent,fire_num>::inverse_kinematics(const GCodeState_t &gm, const float target[AXES], const float position[AXES], const float start_velocity, const float end_velocity, const float segment_time, float steps[MOTORS]) {
+    // The plan:
+    // 1. Call the parent kinematics to get the step count set for all the other motors
+    // 2. Replace the step count for the laser_motor with the number of pulses (if any) for this segment
+    // 3. Precompute the next_ticks_per_pulse for loading into ticks_per_pulse during enable()
+
+    KinematicsParent::inverse_kinematics(gm, target, position, start_velocity, end_velocity, segment_time, steps);
+
+    float move_length = 0;
+    next_ticks_per_pulse = 0;
+
+    // ONLY fire the laser for G1, G2, or G3, when M3 is on, and S > 0
+    if (!paused && ((gm.motion_mode == MOTION_MODE_STRAIGHT_FEED) || (gm.motion_mode == MOTION_MODE_CW_ARC) || (gm.motion_mode == MOTION_MODE_CCW_ARC)) && (gm.spindle_speed > min_s)) {
+        // temporary ticks per pulse
+        float ticks_in_move = segment_time * (60 * FREQUENCY_DDA);
+        float tpp = pulse_duration_us / (1000000 / FREQUENCY_DDA);
+
+        if ((gm.spindle_direction == /*M4*/ SPINDLE_CCW) ||
+            (gm.spindle_direction == /*M3*/ SPINDLE_CW && gm.tool == 1)) {
+            // "Constant power" - the whole move will have the same rate of power, and it'll be a % from 0 to 100
+            // Assume X/Y plane for now, also assume we don't need to worry about any encoder compensation that was done in the parent kinematics
+            float s = std::min(1.0f, std::max(0.0f, ((gm.spindle_speed - min_s)/(max_s-min_s)) ));
+
+            // TODO: Here we would apply some dot-gain curve to s
+
+            move_length = (ticks_in_move/tpp) * s;
+        } else if ((gm.spindle_direction == /*M3*/SPINDLE_CW)) {
+            // Assume X/Y plane for now, also assume we don't need to worry about any encoder compensation that was done in the parent kinematics
+            float x_len = position[AXIS_X] - target[AXIS_X];
+            float y_len = position[AXIS_Y] - target[AXIS_Y];
+
+            float s = ((gm.spindle_speed - min_s)/(max_s-min_s)) * (max_ppm - min_ppm)  + min_ppm;
+
+            move_length = sqrt((x_len * x_len) + (y_len * y_len)) * s;
+            // NOTE: segment_time is in minutes
+            // Ensure that it can only pulse as many pulses as are availabable in the segment time
+            move_length = std::min(move_length, ticks_in_move);
+        }
+
+        //compute the pulse duration, and ensure that it can't be longer than 100% of the time available for each pulse, plus 1 (to ensure it stays on)
+        next_ticks_per_pulse = std::min((uint16_t)tpp, ((uint16_t)(move_length/ticks_in_move+1)));
+    }
+
+
+    // Reminder: steps is *continous* it's moved to from the stepr returned the last time this was called
+    laser_step_position += move_length;
+    steps[laser_motor] = laser_step_position;
+}
 
 #endif  // End of include guard: LASER_TOOLHEAD_H_ONCE
